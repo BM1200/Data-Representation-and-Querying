61476df137a85cf95af3007cbe4935a709dcfbcc
BM1200 <mcshanebrendan@yahoo.com>
2023-09-22 10:55:39 +0000

    exercise one

---
+++ Lab 0/exercise1.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+function SayHello()
+{
+    console.log("Hello");
+}
+
+SayHello();
+
+const myFunction = function SayHi(){
+    console.log("Hi");
+}
+
+myFunction();
