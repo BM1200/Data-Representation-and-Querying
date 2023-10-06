0d4e404dbfbdc67f426eb14172e44759c249936f
BM1200 <mcshanebrendan@yahoo.com>
2023-10-06 10:41:14 +0000

    added header file

---
+++ Lab 2/book-app/src/components/header.js
@@ -0,0 +1,11 @@
+function Header(){
+    
+    return (
+        <div className="Content">
+         <h2>My Header in another component.</h2>
+         
+        </div>
+      );
+}
+
+export default Header;
